8375e9d52117246af145388c859b0d79126ab7bf
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2023-12-08 19:24:32 +0700

    added fragment hero

---
+++ resources/js/Components/Fragment/Hero.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import TextInput from "../TextInput";
+
+const Hero = ({ children }) => {
+    return (
+        <div className="flex flex-nowrap w-full mt-8 justify-between">
+            {children}
+        </div>
+    );
+};
+const HeroLeft = ({title, subtitle, location, type, placeholder}) => {
+    return (
+        <div className="w-full md:w-2/3 order-2 md:order-1">
+            <div className="w-full md:pr-8 h-full flex justify-between flex-col">
+                <div className="flex flex-col mt-16">
+                    <h1 className="text-secondary font-bold text-4xl">
+                        {title}
+                    </h1>
+                    <p className="font-medium text-dark text-xl">
+                        {subtitle}
+                    </p>
+                </div>
+                <div className="md:pr-8">
+                    <h6 className="text-dark mb-2">
+                        Lokasi Anda:{" "}
+                        <span className="font-semibold"> {location}</span>
+                    </h6>
+                    <TextInput
+                        type={type}
+                        placeholder={placeholder}
+                        className="w-full"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const HeroRight = () => {
+    return (
+            <div className="w-full order-1 md:order-2 md:w-1/3 h-full">
+                <img
+                    src="images/app/hero_home.webp"
+                    className="rounded h-full w-full"
+                    alt=""
+                />
+            </div>
+    );
+};
+
+Hero.HeroLeft = HeroLeft;
+Hero.HeroRight = HeroRight;
+
+export default Hero;
--- resources/js/Pages/Home/Index.jsx
+++ resources/js/Pages/Home/Index.jsx
@@ -1,6 +1,6 @@
 import Container from "@/Components/Container";
-import Footer from "@/Components/Footer";
-import TextInput from "@/Components/TextInput";
+
+import Hero from "@/Components/Fragment/Hero";
 import Home from "@/Layouts/Home";
 import { Head } from "@inertiajs/react";
 
@@ -11,49 +11,16 @@ export default function Index() {
 
             {/* Hero Start */}
             <Container>
-                <section>
-                    <div className="flex flex-wrap w-full mt-8 justify-between">
-                        <div className="w-full md:w-2/3 order-2 md:order-1">
-                            <div className="w-full md:pr-8 h-full flex justify-between flex-col">
-                                <div className="flex flex-col mt-16">
-                                    <h1 className="text-secondary font-bold text-4xl">
-                                        GEO-MPASI
-                                    </h1>
-                                    <p className="font-medium text-dark text-xl">
-                                        Mari bersama-sama mencegah{" "}
-                                        <span className="font-semibold">
-                                            Growth Faltering{" "}
-                                        </span>
-                                        di{" "}
-                                        <span className="font-semibold">
-                                            Indonesia
-                                        </span>
-                                    </p>
-                                </div>
-                                <div className="md:pr-8">
-                                    <h6 className="text-dark mb-2">
-                                        Lokasi Anda:{" "}
-                                        <span className="font-semibold">
-                                            {" "}
-                                            Yogyakarta
-                                        </span>
-                                    </h6>
-                                    <TextInput
-                                        type="text" placeholder="Masukkan Lokasi Anda"
-                                        className="w-full"
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="w-full order-1 md:order-2 md:w-1/3 h-full">
-                            <img
-                                src="images/app/hero_home.webp"
-                                className="rounded h-full w-full"
-                                alt=""
-                            />
-                        </div>
-                    </div>
-                </section>
+                <Hero>
+                    <Hero.HeroLeft
+                        title="GEO-MPASI"
+                        subtitle="Mari bersama-sama mencegah Growth Faltering di Indonesia"
+                        location="Yogyakarta"
+                        placeholder="Masukkan Lokasi Anda"
+                        type="search"
+                    ></Hero.HeroLeft>
+                    <Hero.HeroRight></Hero.HeroRight>
+                </Hero>
             </Container>
             {/* Hero End */}
         </>
