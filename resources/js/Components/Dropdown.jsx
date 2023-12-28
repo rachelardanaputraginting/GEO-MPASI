29e311a18f2ee92223c05aeb87c314665c464754
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2023-12-29 05:00:38 +0700

    feat: add dropdown in navbar chat app

--- resources/js/Components/Dropdown.jsx
+++ resources/js/Components/Dropdown.jsx
@@ -30,7 +30,7 @@ const Trigger = ({ children }) => {
     );
 };
 
-const Content = ({ align = 'right', width = '48', contentClasses = 'py-1 bg-white', children }) => {
+const Content = ({ align = 'right', width = '48', contentClasses = 'py-1 bg-white', widthClasses = 'w-48', children }) => {
     const { open, setOpen } = useContext(DropDownContext);
 
     let alignmentClasses = 'origin-top';
@@ -41,7 +41,6 @@ const Content = ({ align = 'right', width = '48', contentClasses = 'py-1 bg-whit
         alignmentClasses = 'origin-top-right right-0';
     }
 
-    let widthClasses = '';
 
     if (width === '48') {
         widthClasses = 'w-48';
@@ -60,7 +59,7 @@ const Content = ({ align = 'right', width = '48', contentClasses = 'py-1 bg-whit
                 leaveTo="transform opacity-0 scale-95"
             >
                 <div
-                    className={`absolute z-50 mt-2 rounded shadow-lg ${alignmentClasses} ${widthClasses}`}
+                    className={`absolute z-50 mt-4 rounded shadow-lg ${alignmentClasses} ${widthClasses}`}
                     onClick={() => setOpen(false)}
                 >
                     <div className={`rounded-md ring-1 ring-black ring-opacity-5 ` + contentClasses}>{children}</div>
@@ -75,7 +74,7 @@ const DropdownLink = ({ className = '', children, ...props }) => {
         <Link
             {...props}
             className={
-                'block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out ' +
+                'block w-full px-4 py-2 text-left text-sm leading-5 text-dark hover:bg-third focus:outline-none focus:bg-third transition duration-150 ease-in-out ' +
                 className
             }
         >
--- resources/js/Layouts/Chat.jsx
+++ resources/js/Layouts/Chat.jsx
@@ -1,3 +1,4 @@
+import Dropdown from "@/Components/Dropdown";
 import { Head, Link, usePage } from "@inertiajs/react";
 import React from "react";
 
@@ -8,24 +9,67 @@ export default function Chat({ title, children }) {
             <Head title={title} />
             <div className="w-1/4">
                 <div className="w-1/4 fixed flex flex-col h-full text-left border-r border-fifth space-y-2">
-                    <div className="shadow px-4 py-4 bg-primary">
-                        <div className="flex gap-2">
-                            <img
-                                src={
-                                    `${auth.user.picture}`
-                                        ? `/storage/${auth.user.picture}`
-                                        : "https://www.pngplay.com/wp-content/uploads/12/User-Avatar-Profile-PNG-Free-File-Download.png"
-                                }
-                                alt={auth.user.picture}
-                                className="h-10 w-10 rounded"
-                            />
+                    <div className="shadow-xl px-4 py-2 bg-primary border-b border-fifth">
+                        <div className="w-full flex justify-start sm:flex sm:items-center">
+                            {auth.user ? (
+                                <Dropdown>
+                                    <Dropdown.Trigger>
+                                        <span className="inline-flex rounded-md">
+                                            <button
+                                                type="button"
+                                                className="inline-flex items-center px-3 py-2 leading-4 font-medium rounded-md text-white bg-primary hover:text-dark focus:outline-none transition ease-in-out duration-150"
+                                            >
+                                                <div className="flex items-center gap-2">
+                                                    <img
+                                                        src={
+                                                            `${auth.user.picture}`
+                                                                ? `/storage/${auth.user.picture}`
+                                                                : "https://www.pngplay.com/wp-content/uploads/12/User-Avatar-Profile-PNG-Free-File-Download.png"
+                                                        }
+                                                        alt={auth.user.picture}
+                                                        className="h-10 w-10 rounded"
+                                                    />
+                                                    {auth.user.name}
+                                                    <svg
+                                                        className="ml-2 -mr-0.5 h-4 w-4"
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        viewBox="0 0 20 20"
+                                                        fill="currentColor"
+                                                    >
+                                                        <path
+                                                            fillRule="evenodd"
+                                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                            clipRule="evenodd"
+                                                        />
+                                                    </svg>
+                                                </div>
+                                            </button>
+                                        </span>
+                                    </Dropdown.Trigger>
 
-                            <div className="flex flex-col">
-                                <div className="font-semibold">
-                                    {auth.user.name}
-                                </div>
-                                <div className="text-xs">Rs. Sejahtera</div>
-                            </div>
+                                    <Dropdown.Content align="left" width="48">
+                                        <Dropdown.Link
+                                            href={route("home.index")}
+                                        >
+                                            Beranda
+                                        </Dropdown.Link>
+                                        <Dropdown.Link
+                                            href={route("profile.edit")}
+                                        >
+                                            Profil Saya
+                                        </Dropdown.Link>
+                                        <Dropdown.Link
+                                            href={route("logout")}
+                                            method="post"
+                                            as="button"
+                                        >
+                                            Log Out
+                                        </Dropdown.Link>
+                                    </Dropdown.Content>
+                                </Dropdown>
+                            ) : (
+                                ""
+                            )}
                         </div>
                     </div>
                     <div className="flex-1 overflow-y-auto p-4 space-y-3">
@@ -62,17 +106,6 @@ export default function Chat({ title, children }) {
                             </Link>
                         ))}
                     </div>
-                    {/* <div className="bg-dark rounded-xl p-4 space-y-3">
-                        <div className="text-white">{auth.user.name}</div>
-                        <Link
-                            className="border bg-white font-medium text-black rounded-xl px-4 py-2"
-                            as="button"
-                            method="POST"
-                            href={route("logout")}
-                        >
-                            Log out
-                        </Link>
-                    </div> */}
                 </div>
             </div>
             <div className="w-3/4">{children}</div>
