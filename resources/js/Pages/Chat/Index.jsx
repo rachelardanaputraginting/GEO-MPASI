e2cfb0b08270bb624ddc7ddb7f299453991fe0bc
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2023-12-29 05:05:54 +0700

    update design start chat in chat app

--- resources/js/Pages/Chat/Index.jsx
+++ resources/js/Pages/Chat/Index.jsx
@@ -1,8 +1,19 @@
 import Chat from "@/Layouts/Chat";
+import { IconBrandWhatsapp, IconMessage } from "@tabler/icons-react";
 import React from "react";
 
 export default function Index() {
-    return <div className="px-6 py-4">Ayo mulai obrolan</div>;
+    return (
+        <div className="px-6 py-4 flex flex-col items-center justify-center flex-1 h-screen space-y-2">
+            <p className="text-lg text-fifth font-semibold">
+                Ayoo mulai obrolan
+            </p>
+            <p className="text-xl text-fifth">
+                Silahkan berdiskusi dengan Dokter kesayangan Anda
+            </p>
+            <IconMessage className="text-fifth" size={72} stroke={1} />
+        </div>
+    );
 }
 
 Index.layout = (page) => <Chat children={page} title="Chattan App" />;
